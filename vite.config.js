8e108ae6ec3ace879e661c27c89a313692c7ecfa
Gayatri Chippawar <gayatrichippawar@gmail.com>
2025-07-13 19:37:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fallback from 'vite-plugin-mpa-fallback'
 
 // https://vite.dev/config/
 export default defineConfig({
