8261bc63e98019f01588e46431a4e59a4e31a1ea
Gayatri Chippawar <gayatrichippawar@gmail.com>
2025-07-17 11:26:07 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://intervyou.onrender.com/api',
+  baseURL: 'https://intervyou.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
